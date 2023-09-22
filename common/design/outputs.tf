fb9b5f418875b94564be4de692a90bc94ce84582
Chris Geroux <chris.m.geroux@gmail.com>
2023-09-22 14:01:15 -0300

    init mC config

---
+++ common/design/outputs.tf
@@ -0,0 +1,19 @@
+output "instances" {
+  value = local.instances
+}
+
+output "instances_to_build" {
+  value = local.instances_to_build
+}
+
+output "volumes" {
+  value = local.volumes
+}
+
+output "volume_per_instance" {
+  value = local.volume_per_instance
+}
+
+output "domain_name" {
+  value = local.domain_name
+}
---
+++ common/design/variables.tf
@@ -0,0 +1,5 @@
+variable "cluster_name" { }
+variable "domain" { }
+variable "instances" { }
+variable "volumes" { }
+variable "pool" { }
